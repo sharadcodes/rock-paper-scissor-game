4c11c34aa833fb559572288ce3365fda58cb81ae
SHARAD RAJ <36638057+sharadcodes@users.noreply.github.com>
2019-06-05 20:22:40 +0530

    Add files via upload

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -1,44 +1,66 @@
-var rock_img = document.getElementById("r");
-var paper_img = document.getElementById("p");
-var scissors_img = document.getElementById("s");
-
+var rock_img = document.getElementById("rock");
+var paper_img = document.getElementById("paper");
+var scissors_img = document.getElementById("scissors");
 var computer_score = document.getElementById("computer-score");
 var human_score = document.getElementById("human-score");
 var winner = document.getElementById("winner");
-var declaration = document.getElementById("score-seperator");
+var choices = document.getElementById("choices");
 var announcer = document.getElementById("announce");
 
 var game_level = 1;
-var status = 0;
-var pressed_div = "";
-
 const max = 3;
 var c_total = 0;
 var h_total = 0;
 
-function result(user_value) {
+var list_of_choices = ["Rock", "Paper", "Scissor"];
+var computerChoice;
+
+function result(user_value, computer_value) {
+  chance();
   if (game_level < 11) {
-    var c = getRandomInt();
-    var h = getRandomInt(user_value);
-    if (c < h) {
+    if (
+      (user_value === "rock" && computer_value === "scissors") ||
+      (user_value === "scissors" && computer_value === "paper") ||
+      (user_value === "paper" && computer_value === "rock")
+    ) {
       h_total++;
-      status = 1;
-      highlight(pressed_div, status);
-      console.log("HUMAN");
-    } else if (c > h) {
+      var status = 1;
+      choices.innerHTML =
+        "<span>You showed <i style=' color: yellow;'>" +
+        user_value +
+        "</i> Computer showed <i style=' color: yellow;'> " +
+        computer_value;
+      highlight(user_value, status);
+    } else if (
+      (user_value === "paper" && computer_value === "scissors") ||
+      (user_value === "rock" && computer_value === "paper") ||
+      (user_value === "scissors" && computer_value === "rock")
+    ) {
       c_total++;
-      status = 0;
-      highlight(pressed_div, status);
-      console.log("bot");
+      var status = 0;
+      choices.innerHTML =
+        "<span>You showed <i style=' color: yellow;'>" +
+        user_value +
+        "</i> Computer showed <i style=' color: yellow;'>" +
+        computer_value;
+      highlight(user_value, status);
+    } else if (
+      (user_value === "rock" && computer_value === "rock") ||
+      (user_value === "paper" && computer_value === "paper") ||
+      (user_value === "scissors" && computer_value === "scissors")
+    ) {
+      var status = -1;
+      choices.innerHTML =
+        "<span>You showed <i style=' color: yellow;'>" +
+        user_value +
+        "</i> Computer showed <i style=' color: yellow;'>" +
+        computer_value;
+      highlight(user_value, status);
     } else {
-      status = -1;
-      highlight(pressed_div, status);
-      console.log("DRAW");
     }
     computer_score.innerText = c_total;
     human_score.innerText = h_total;
   }
-  chance();
   game_level++;
   check_level(game_level);
 }
@@ -47,6 +69,10 @@ function result(user_value) {
 
 function getRandomInt() {
   return Math.floor(Math.random() * Math.floor(max));
+}
+
+function getRandomIntUser(value) {
+  return Math.floor(Math.random() * Math.floor(value));
 }
 
 // Chances
@@ -58,19 +84,18 @@ function chance() {
   }
 }
 
-function game(userChoice) {
-  pressed_div = userChoice;
+function game(userChoice, computerChoice) {
   switch (userChoice) {
-    case "r": {
-      result(1);
+    case "rock": {
+      result("rock", computerChoice);
       break;
     }
-    case "p": {
-      result(2);
+    case "paper": {
+      result("paper", computerChoice);
       break;
     }
-    case "s": {
-      result(3);
+    case "scissors": {
+      result("scissors", computerChoice);
       break;
     }
     default:
@@ -79,94 +104,110 @@ function game(userChoice) {
 }
 
 function highlight(my_value, flag) {
-  if (flag == 1) {
+  if (flag === 1) {
     switch (my_value) {
-      case "r":
+      case "rock":
         rock_img.classList.add("green");
         setTimeout(() => rock_img.classList.remove("green"), 400);
         break;
-      case "s":
+      case "scissors":
         scissors_img.classList.add("green");
         setTimeout(() => scissors_img.classList.remove("green"), 400);
         break;
-      case "p":
+      case "paper":
         paper_img.classList.add("green");
         setTimeout(() => paper_img.classList.remove("green"), 400);
         break;
       default:
         break;
     }
-  } else if (flag == 0) {
+  } else if (flag === 0) {
     switch (my_value) {
-      case "r":
+      case "rock":
         rock_img.classList.add("red");
         setTimeout(() => rock_img.classList.remove("red"), 400);
         break;
-      case "s":
+      case "scissors":
         scissors_img.classList.add("red");
         setTimeout(() => scissors_img.classList.remove("red"), 400);
         break;
-      case "p":
+      case "paper":
         paper_img.classList.add("red");
         setTimeout(() => paper_img.classList.remove("red"), 400);
         break;
       default:
         break;
     }
-  } else if (flag == -1) {
+  } else if (flag === -1) {
     switch (my_value) {
-      case "r":
+      case "rock":
         rock_img.classList.add("grey");
         setTimeout(() => rock_img.classList.remove("grey"), 400);
         break;
-      case "s":
+      case "scissors":
         scissors_img.classList.add("grey");
         setTimeout(() => scissors_img.classList.remove("grey"), 400);
         break;
-      case "p":
+      case "paper":
         paper_img.classList.add("grey");
         setTimeout(() => paper_img.classList.remove("grey"), 400);
         break;
       default:
         break;
     }
+  } else {
   }
 }
 
 // MAIN
+function main() {
+  rock_img.addEventListener("click", function() {
+    game("rock", generate_C_choice());
+  });
 
-rock_img.addEventListener("click", function() {
-  game("r");
-});
+  paper_img.addEventListener("click", function() {
+    game("paper", generate_C_choice());
+  });
 
-paper_img.addEventListener("click", function() {
-  game("p");
-});
-
-scissors_img.addEventListener("click", function() {
-  game("s");
-});
-
-
+  scissors_img.addEventListener("click", function() {
+    game("scissors", generate_C_choice());
+  });
+}
 
 // check level
 
-function check_level(level){
-  if(level == 11){
-  var child1 = document.getElementById("r");
-  var child2 = document.getElementById("p");
-  var child3 = document.getElementById("s");
-  child1.parentNode.removeChild(child1);
-  child2.parentNode.removeChild(child2);
-  child3.parentNode.removeChild(child3);
-  if (h_total > c_total) {
-    announcer.innerText = "Hurray! You are the champion !";
-  } else if (h_total < c_total) {
-    announcer.innerText = "So sad you lost !";
-  } else if (h_total == c_total) {
-    announcer.innerText = "Scores tied it's a Draw";
-  } else {
-    announcer.innerText = "Something is wrong!";
+function check_level(level) {
+  if (level === 11) {
+    var child1 = document.getElementById("rock");
+    var child2 = document.getElementById("paper");
+    var child3 = document.getElementById("scissors");
+    child1.parentNode.removeChild(child1);
+    child2.parentNode.removeChild(child2);
+    child3.parentNode.removeChild(child3);
+    if (h_total > c_total) {
+      announcer.innerText = "Hurray! You are the champion !";
+    } else if (h_total < c_total) {
+      announcer.innerText = "So sad you lost !";
+    } else if (h_total === c_total) {
+      announcer.innerText = "Scores tied it's a Draw";
+    } else {
+      announcer.innerText = "Something is wrong!";
+    }
   }
 }
+
+function generate_C_choice() {
+  var random_value = getRandomInt();
+  var z = "";
+
+  if (random_value === 1) {
+    z = "rock";
+  } else if (random_value === 2) {
+    z = "paper";
+  } else if (random_value === 3) {
+    z = "scissors";
+  } else {
+  }
+  return z;
 }
+main();
